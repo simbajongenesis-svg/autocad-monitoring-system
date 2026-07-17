1f5eeb68750dcd5aca40dae6cc2817411a476e39
unknown <simbajongenesi@gmail.com>
2026-07-17 14:14:05 +0800

    Stable version after successful deployment

--- autocad_system/settings.py
+++ autocad_system/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,12 +21,17 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-(em#dzjtz^ks6k)3lak01fv6--oi#!1&tydd$2%z*)$9af(ff('
+import os
+
+SECRET_KEY = os.environ.get(
+    "SECRET_KEY",
+    "django-insecure-(em#dzjtz^ks6k)3lak01fv6--oi#!1&tydd$2%z*)$9af(ff("
+)
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["autocad-monitoring-system.onrender.com", "127.0.0.1", "localhost"]
 
 
     
